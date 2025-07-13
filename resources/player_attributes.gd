6e399ce6f2452eff0b367aa620127d713a9af54c
Kidus <kidzegeye@gmail.com>
2025-07-12 17:20:44 -0700

    add dive reticle

--- resources/player_attributes.gd
+++ resources/player_attributes.gd
@@ -1,10 +1,13 @@
 extends Resource
 class_name PlayerAttributes
 
-@export var SPEED: float = 130.0
+@export var SPEED: float = 150.0
 @export var JUMP_VELOCITY: float = -300.0
-@export var DIVE_VELOCITY: float = -400.0
+@export var DIVE_VELOCITY: float = 300.0
 @export var MAX_JUMPS: int = 2
 @export var MAX_DIVES: int = 2
-@export var MAX_JUMP_COOLDOWN: float = 500.0
-@export var MAX_DIVE_COOLDOWN: float = 500.0
+@export var MAX_JUMP_COOLDOWN: float = 50.0
+@export var MAX_DIVE_COOLDOWN: float = 250.0
+@export var DIVE_CHARGE_RATE: float = 2
+@export var RETICLE_MIN_DISTANCE: float = 10
+@export var RETICLE_MAX_DISTANCE: float = 50
--- scripts/player.gd
+++ scripts/player.gd
@@ -4,25 +4,36 @@ extends CharacterBody2D
 
 var dead: bool = false
 var diving: bool = false
+var charging_dive: bool = false
 
 var jumps_used: int = 0
 var dives_used: int = 0
 var jump_cooldown: float = 0.0
 var dive_cooldown: float = 0.0
 var direction: float = 0
+var charge_dive_percent: float = 0.0
 
 signal hasDied()
 
 @onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
 @onready var animation_player: AnimationPlayer = $AnimatedSprite2D/AnimationPlayer
+@onready var reticle: Polygon2D = $Reticle
+var default_reticle
 
-func _onready():
+func _ready():
 	diving = false
 	dead = false
+	charging_dive = false
+	default_reticle = reticle.polygon.duplicate()
+	reticle.hide()
+	
 	
 func _physics_process(delta: float) -> void:
 	#print("diving: %s, dives_used: %d, dive_cooldown: %d, rotation: %f, is_on_floor: %s" % [diving, dives_used, dive_cooldown, animated_sprite.rotation, is_on_floor()])
-
+	if attr.RETICLE_MAX_DISTANCE*charge_dive_percent < attr.RETICLE_MIN_DISTANCE:
+		reticle.hide()
+	else:
+		reticle.show()
 	gravity_handler(delta)
 	direction = 0
 	if not dead:
@@ -42,28 +53,49 @@ func gravity_handler(delta):
 func dive_handler(delta):
 	# Handle diving
 	if diving:
-		# Face the dive
-		look_at(velocity)
+		# Face the dived
+		animated_sprite.look_at(velocity)
+		motion_mode = CharacterBody2D.MOTION_MODE_GROUNDED
 
 	if dive_cooldown > 0.0:
 		dive_cooldown -= delta * 1000 # convert to ms
+		
 	if is_on_floor() && dives_used > 0: # we know we've dived 
-		rotation = 0.0
+		animated_sprite.rotation = 0.0
 		animated_sprite.play("dive_end")
 		dives_used = 0
 		diving = false
-	if Input.is_action_just_pressed("dive") and dives_used < attr.MAX_DIVES && dive_cooldown <= 0.0:
+		
+	if Input.is_action_just_pressed("dive") && dives_used < attr.MAX_DIVES && dive_cooldown <= 0.0:
+		charging_dive = true
+		
+	if charging_dive:
+		if charge_dive_percent < 1.0:
+			charge_dive_percent += delta * attr.DIVE_CHARGE_RATE
+			
+		# Charge reticle
+		reticle.polygon[2] = reticle.to_local(get_global_mouse_position()).normalized() * max(attr.RETICLE_MIN_DISTANCE, attr.RETICLE_MAX_DISTANCE * charge_dive_percent)
+		reticle.rotation = reticle.position.angle_to_point(get_local_mouse_position())
+		reticle.modulate.s = 1 * charge_dive_percent
+		
+	if Input.is_action_just_released("dive") && dives_used < attr.MAX_DIVES && dive_cooldown <= 0.0 && charging_dive:
 		var dive_direction: Vector2 = (get_global_mouse_position() - global_position).normalized()
 		animation_player.queue("dive_start")
-		velocity = dive_direction * attr.DIVE_VELOCITY
+		velocity = dive_direction * attr.DIVE_VELOCITY * (0.7 + charge_dive_percent)
 		print("%s | %s" % [dive_direction, velocity])
-
-		look_at(velocity)
+		
+		motion_mode = CharacterBody2D.MOTION_MODE_FLOATING
+		animated_sprite.look_at(velocity)
 		animated_sprite.flip_h = sign(velocity.x) < 0
 		
 		dives_used += 1
 		diving = true
 		dive_cooldown = attr.MAX_DIVE_COOLDOWN
+		charging_dive = false
+		charge_dive_percent = 0
+		reticle.rotation = 0
+		reticle.polygon = default_reticle
+		
 		
 func jump_handler(delta):
 	# Handle jump.
@@ -109,5 +141,5 @@ func die():
 		print("death")
 		hasDied.emit()
 		dead = true
-		rotation = 0.0
+		animated_sprite.rotation = 0.0
 		animated_sprite.play("death")
